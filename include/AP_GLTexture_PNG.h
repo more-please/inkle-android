9bec8d9ed057bac3a8b8b97784b14e7ad7b1828b
Iain Merrick <iain@iainmerrick.com>
2013-11-19 19:58:30 +0000

    AP_GLTexture_PNG

---
+++ include/AP_GLTexture_PNG.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+#import "AP_GLTexture.h"
+
+// Loads a texture from a PNG file.
+// Mipmaps are generated automatically.
+
+@interface AP_GLTexture_PNG : AP_GLTexture
+
+- (AP_GLTexture_PNG*) initWithData:(NSData*)data;
+
++ (BOOL) isPNG:(NSData*)data;
++ (AP_GLTexture*) withData:(NSData*)data;
+
+@end
