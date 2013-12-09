e2638edb047ace70869f6b812ecb728a36c44065
Iain Merrick <iain@iainmerrick.com>
2013-12-09 16:45:49 +0000

    Text shadow rendering

--- include/AP_Font_Run.h
+++ include/AP_Font_Run.h
@@ -14,8 +14,6 @@
 
 @property(nonatomic) CGPoint origin;
 @property(nonatomic) UIColor* textColor; // default is nil (text draws black)
-@property(nonatomic) UIColor* shadowColor; // default is nil (no shadow)
-@property(nonatomic) CGSize shadowOffset; // default is CGSizeMake(0, -1) -- a top shadow
 
 - (AP_Font_Run*) initWithData:(AP_Font_Data*)data pointSize:(CGFloat)pointSize glyphs:(unsigned char*)glyphs length:(size_t)length;
 
@@ -23,5 +21,6 @@
 - (AP_Font_Run*) splitAtLineBreakLeaving:(AP_Font_Run**)leftover;
 
 - (void) renderWithBoundsToGL:(CGAffineTransform)boundsToGL alpha:(CGFloat)alpha;
+- (void) renderWithBoundsToGL:(CGAffineTransform)boundsToGL color:(GLKVector4)rgba;
 
 @end
