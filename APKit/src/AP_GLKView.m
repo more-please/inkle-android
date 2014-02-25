7318284aba86a56904e64063a0d6b5e9704c8e07
Iain Merrick <iain@iainmerrick.com>
2014-02-25 12:12:49 +0000

    Fix map bounds (it should slide up when opening the spellbook)

--- APKit/src/AP_GLKView.m
+++ APKit/src/AP_GLKView.m
@@ -1,5 +1,7 @@
 #import "AP_GLKView.h"
 
+#import <OpenGLES/ES2/gl.h>
+
 #import "AP_Check.h"
 
 @implementation AP_GLKView
@@ -16,8 +18,32 @@
 {
     [super renderWithBoundsToGL:boundsToGL alpha:alpha];
 
+    // Bit of a hack here... adjust the GL viewport to compensate for our view frame.
+    // It would be better to adjust the projection matrix, but that would require
+    // changes to SceneController (plus, getting those matrices right is hard!)
+
+    UIScreen* screen = [UIScreen mainScreen];
+    float scale = screen.scale;
+    CGRect bounds = screen.bounds;
+
+    CGRect frame = [self convertInFlightRect:self.inFlightBounds toView:nil];
+    CGPoint offset = {
+        frame.origin.x - bounds.origin.x,
+        frame.origin.y - bounds.origin.y,
+    };
+
+    glViewport(
+        offset.x * scale,
+        -offset.y * scale, // GL origin is at the bottom-left
+        bounds.size.width * scale,
+        bounds.size.height * scale);
+
+    // Draw!
     AP_CHECK(_delegate, return);
     [_delegate glkView:self drawInRect:self.bounds];
+
+    // Restore the normal viewport.
+    glViewport(0, 0, bounds.size.width * scale, bounds.size.height * scale);
 }
 
 @end
