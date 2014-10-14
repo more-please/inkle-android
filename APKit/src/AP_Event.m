1c2a692433049e8d109c949197f0835cab8509fe
Iain Merrick <iain@iainmerrick.com>
2014-10-14 12:40:35 +0100

    [AP_Event touchesForView:]

--- APKit/src/AP_Event.m
+++ APKit/src/AP_Event.m
@@ -2,4 +2,12 @@
 
 @implementation AP_Event
 
+- (NSSet*) touchesForView:(AP_View*)view
+{
+    // This is only used (in 80 Days) for ItemView, for drag tracking.
+    // Just return all the touches, which will only cause minor problems
+    // in the unlikely case of a multi-touch drag.
+    return _allTouches;
+}
+
 @end
