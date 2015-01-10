97045e916ad7a645f4c18a51b41eba695fcc8993
Iain Merrick <iain@iainmerrick.com>
2015-01-10 11:21:29 +0000

    Make sure AP_GetDelegate has C linkage

--- APKit/include/APKit/APKit_main.h
+++ APKit/include/APKit/APKit_main.h
@@ -2,5 +2,13 @@
 
 #import "AP_Application.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Must be provided by application-specific code
 extern id<AP_ApplicationDelegate> AP_GetDelegate();
+
+#ifdef __cplusplus
+}
+#endif
