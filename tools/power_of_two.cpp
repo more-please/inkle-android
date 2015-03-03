cf51b24d4f757d37d951250524e1fb9336c516ec
Iain Merrick <iain@iain-pro.local>
2015-03-03 16:05:10 +0000

    Add --max flag to power_of_two (default 4096)

--- tools/power_of_two.cpp
+++ tools/power_of_two.cpp
@@ -14,7 +14,8 @@ void usage() {
         "Scale the input up (or slightly down) so its dimensions are exact powers of two.\n\n"
         "Usage: power_of_two [-i in.png] [-o out.png]\n\n"
         "  -i, --infile: input file (default is stdin)\n"
-        "  -o, --outfile: output file (default is stdout)\n");
+        "  -o, --outfile: output file (default is stdout)\n"
+        "  -m, --max: maximum size (default 4096)\n");
     fflush(stderr);
     exit(1);
 }
@@ -26,13 +27,15 @@ bool isFlag(const char* s, const char* flag1, const char* flag2) {
 int main(int argc, const char* argv[]) {
     const char* infile = NULL;
     FILE* outfile = stdout;
-    int width = 0, height = 0, bpp = 0;
+    int width = 0, height = 0, bpp = 0, maxsize = 4096;
     for (int i = 1; i < argc; ++i) {
         const char* s = argv[i];
         if (isFlag(s, "-i", "--infile")) {
             infile = argv[++i];
         } else if (isFlag(s, "-o", "--outfile")) {
             outfile = fopen(argv[++i], "wb");
+        } else if (isFlag(s, "-m", "--max")) {
+            maxsize = atoi(s);
         } else {
             fprintf(stderr, "Bad argument '%s'\n\n", s);
             usage();
@@ -46,8 +49,12 @@ int main(int argc, const char* argv[]) {
 
     int w2 = 4, h2 = 4;
     double k = 1.25; // Allow images to be shrunk by this amount
-    while (k * w2 < w) w2 *= 2;
-    while (k * h2 < h) h2 *= 2;
+    while (k * w2 < w && w2 < maxsize) {
+        w2 *= 2;
+    }
+    while (k * h2 < h && h2 < maxsize) {
+        h2 *= 2;
+    }
 
     unsigned char* output;
     if (w2 == w && h2 == h) {
