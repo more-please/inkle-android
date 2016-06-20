4c276af1ac4035c278da43515a2b18082d0e9bd2
Iain Merrick <iain@iain-pro.local>
2016-06-20 17:07:36 -0500

    Fuck it, just call glGetError everywhere (until I figure out this startup crash)

--- APKit/include/APKit/AP_Check.h
+++ APKit/include/APKit/AP_Check.h
@@ -51,8 +51,8 @@
         } \
     } while(0)
 
-#define _GL(cmd, ...) gl ## cmd(__VA_ARGS__)
-// #define _GL(cmd, ...) do { gl ## cmd(__VA_ARGS__); AP_CHECK_GL(#cmd,); } while(0)
+// #define _GL(cmd, ...) gl ## cmd(__VA_ARGS__)
+#define _GL(cmd, ...) do { gl ## cmd(__VA_ARGS__); AP_CHECK_GL(#cmd,); } while(0)
 // #define _GL(cmd, ...) do { NSLog(@"%s:%d %s", __FILE__, __LINE__, #cmd); gl ## cmd(__VA_ARGS__); AP_CHECK_GL(#cmd,); } while(0)
 
 #define AP_NOT_IMPLEMENTED \
