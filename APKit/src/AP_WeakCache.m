f1a2816966756d04946b2e7945ac62fbe93793f9
Iain Merrick <iain@iainmerrick.com>
2018-03-09 15:08:16 +0000

    Major update to AP_WeakCache to hopefully reduce graphics thrashing, which is possibly contributing to frequent crashes on Adreno phones. Expire cache entries by timestamp, not via weak pointers. Use a generous 10 minute timeout.

--- APKit/src/AP_WeakCache.m
+++ APKit/src/AP_WeakCache.m
@@ -2,15 +2,42 @@
 
 #import "AP_Animation.h"
 #import "AP_Check.h"
+#import "AP_Utils.h"
 
 #import <PAK/PAK.h>
 
 @interface AP_WeakCache_Entry : NSObject
 @property (nonatomic,strong) id key;
-@property (nonatomic,weak) id value;
+@property (nonatomic,strong) id value;
 @end
 
-@implementation AP_WeakCache_Entry
+@implementation AP_WeakCache_Entry {
+    id _value;
+    double _atime;
+}
+
+- (id) value
+{
+    _atime = AP_TimeInSeconds();
+    return _value;
+}
+
+- (void) setValue:(id)v
+{
+    _atime = AP_TimeInSeconds();
+    _value = v;
+}
+
+- (void) maybeExpire
+{
+    double now = AP_TimeInSeconds();
+    double age = now - _atime;
+    if (_value && (age > 600)) {
+        NSLog(@"Expiring cache entry: %@", _key);
+        _value = nil;
+    }
+}
+
 @end
 
 @implementation AP_WeakCache {
@@ -48,10 +75,19 @@
     AP_WeakCache_Entry* entry = [_dict objectForKey:key];
     id result = entry.value;
     if (!result) {
+        // Delete any items that haven't been accessed recently
+        for (id k in _dict) {
+            AP_WeakCache_Entry* e = [_dict objectForKey:k];
+            [e maybeExpire];
+        }
+
+        // Create the new item.
         result = loader();
         if (!result) {
             return nil;
         }
+
+        // Add it to the cache.
         entry = [[AP_WeakCache_Entry alloc] init];
         entry.key = key;
         entry.value = result;
